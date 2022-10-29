61f70ddaa6cd9e0711388fe0a602b72c928682cb
Onuoha Collins <onuohacollinschidubem@gmail.com>
2022-10-29 16:20:30 +0100

    new changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,38 +1,55 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import betacare from '../assets/betaCare.png'
+import banking from '../assets/banking.png'
+import githubfinder from '../assets/githubfinder.png'
+import calculator  from '../assets/calculator.png'
+import colorgen from '../assets/colorgen.png'
+import ani from '../assets/ani.png'
+
+
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: betacare,
+      link: 'https://betacare.ng/',
+      repo: 'https://github.com/ONUOHA92/backgroundGenerator'
+
     },
     {
       id: 2,
-      src: reactParallax,
+      src: banking,
+      link: 'https://elegant-cranachan-aab706.netlify.app/',
+      repo: 'https://github.com/ONUOHA92/backgroundGenerator'
     },
     {
       id: 3,
-      src: navbar,
+      src: githubfinder,
+      link: 'https://gallant-feynman-95ed3f.netlify.app/',
+      repo: 'https://github.com/ONUOHA92/github-search-user-redesign'
     },
     {
       id: 4,
-      src: reactSmooth,
+      src: calculator,
+      link: 'https://rad-salmiakki-938fb9.netlify.app/',
+      repo: 'https://github.com/ONUOHA92/backgroundGenerator'
     },
     {
       id: 5,
-      src: installNode,
+      src: colorgen,
+      link: 'https://xenodochial-nightingale-8345d2.netlify.app/',
+      repo: 'https://github.com/ONUOHA92/backgroundGenerator'
     },
     {
       id: 6,
-      src: reactWeather,
+      src: ani,
+      link: 'https://infallible-heisenberg-67aebd.netlify.app/?',
+      repo: 'https://github.com/ONUOHA92/Animated-form'
     },
   ];
+
+
 
   return (
     <div
@@ -48,7 +65,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, link, repo }) => (
             < div key={id} className=" shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -56,12 +73,15 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+
+                <a href={link} className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
-                </button>
-                <button className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105">
+                </a>
+
+
+                <a href={repo} className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105">
                   Code
-                </button>
+                </a>
               </div>
             </div>
           ))}
